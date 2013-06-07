60fcae83e4fbb7cd72cc69a628c47c3f90e2f748
Nick Ball <nicholaspball@gmail.com>
2013-06-06 22:55:37 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// runs the slider across the site
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
+$(document).ready(function(){
+    // fits iframe videos to fluid column widths
+    $("div.main").fitVids();
+
+    $("img.lazy").lazyload();
+
+    // Expandable sections
+    $('.expandable').expander({
+        expandText: "more",
+        userCollapseText: "less",
+        expandSpeed: 400,
+        collapseSpeed: 400,
+        moreClass: "read-more",
+        lessClass: "read-less",
+        slicePoint: 800
+    });
+
+    //Album cover flips
+    $('.album-block img').click(function() {
+        $(this).parent().parent().toggleClass('flipped');
+    });
+
+    $.localScroll({
+        hash: true,
+        duration: 400
+    });
+
+    //Soundcloud player
+    $(window).load(function() {
+        var width = $(window).width();
+        if (width < 960) {
+            $.stratus({
+                links: 'https://soundcloud.com/middlewestmgmt/sets/middlewestmgmt-com',
+                stats: false
+            });
+            $(".share").css("display", "none");
+        } else {
+            $.stratus({
+                links: 'https://soundcloud.com/middlewestmgmt/sets/middlewestmgmt-com'
+            });
+        }
+    });
+    
+    //Colorbox
+    $('.colorbox-inline').colorbox({
+        inline: true,
+        //width: "50%",
+        rel: 'group'
+    });
+
+});
+
+
+

c55b1ae9bc3de4c552b094ed4c28e2f32515149f
Nick Ball <nicholaspball@gmail.com>
2013-09-14 11:56:45 -0500

    refactored js

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,226 +1,343 @@
-$(function() {
-/*****
-* Colorbox
-******/
-var width = $(window).width();
+var mwm = (function($, window, document) {
 
-if ((width>= 768)) {
+    //Get window width
+    var         winWidth = $(window).width(),
+                  $mobileNavButton = $(" #mobile__menu__button "),
+                  $navUl = $(" nav ul "),
+                  $subNavLink = $(' .sub-nav ul a');
 
-        $(" .colorbox ").colorbox({
-            rel: "group"
+
+    //Check for window width size
+    var isDesk = function() {
+        if (winWidth >= 768) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+    //Mailing List
+    var mailingList = function(email, oid) {
+
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+        if (email == "") {
+            $('.error--invalid').fadeOut(300);
+            $(".error--empty").fadeIn(300);
+            $("input#email").focus();
+            return false;
+        }
+
+        else if (!emailReg.test(email)) {
+            $(".error--empty").fadeOut(100);
+            $(".error--invalid").fadeIn(300);
+            $("input#email").focus();
+            return false;
+        }
+
+        var dataString = $('.mailing-list form').serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "../../php/mailing-list-submit.php",
+            data: dataString,
+            success: function() {
+                var thanks = "<div class='thanks'>Thanks!</div>";
+                $('.error').hide();
+                $(thanks).hide().appendTo("footer").fadeIn(300).delay(1000).fadeOut(300);
+                $('.mailing-list form').each(function() {
+                    this.reset();
+                });
+            },
+            error: function() {
+                $('footer').append("<div class='submit-error error'>There was an error processing this</div>");
+            }
         });
-        $(" .colorbox-iframe ").colorbox({
-            iframe: true,
-            width:"80%",
-            height:"80%",
-            rel: "group-videos",
-            transition: "none",
-            reposition: false
-        });
-
-
-} else {
-    $(" .colorbox ").click(function() {
-        var info = $(this).parent().next();
-        $(info).toggleClass("show");
         return false;
-    });
-    $(" .mobile-album-info-close ").click(function() {
-        $(" .mobile-album-info ").removeClass("show");
-    });
-}
 
+    };
 
-/*****
-* Expander
-******/
-$(" .expandable ").expander({
-    expandText: "more",
-    userCollapseText: "less",
-    expandSpeed: 400,
-    collapseSpeed: 400,
-    moreClass: "read-more",
-    lessClass: "read-less",
-    slicePoint: 800
-});
+    /*****
+    * Tumblr
+    ******/
+    var tumblr = {
+        init: function(tag, page) {
+            var     postCount,
+                      readMore;
 
+            if ( page == "home" && isDesk() ) {
+                postCount = 5;
+                readMore = "<a class='load-more-posts'>Load More Posts</a>";
+            } else if ( page == "home" ) {
+                postCount = 3;
+                readMore = "<a class='load-more-posts'>Load More Posts</a>";
+            } else {
+                postCount = 2;
+                readMore = "<a class='read-more-posts' target='_blank' href='http://middlewestmgmt.tumblr.com/tagged/" + tag +"'>Read More Posts</a>";
+            }
 
-/*****
-* Flexslider
-******/
-$(" .flexslider ").flexslider({
-    animation: "slide",
-    useCSS: true
-});
+            this.getPosts(postCount, tag, readMore);
+        },
 
-// Flexsider for album thumbnails when count > 6
-$(" .flexslider-mini ").flexslider({
-    animation: "slide",
-    slideshow: false,
-    useCSS: true
-});
+        getPosts: function(postCount, tag, readMore) {
 
+            var     offset = $('.news article').length,
+                      count;
 
+            $.ajax({
+                type: 'GET',
+                dataType: 'jsonp',
+                url: 'http://api.tumblr.com/v2/blog/middlewestmgmt.tumblr.com/posts?api_key=5cHAhSpCdCiLOdeoWeJAAwCbqFUW4LCvfe9GxwVJXWcgUH4XSl&offset=' + offset + '&limit=' + postCount + '&tag=' + tag,
+                data: { get_param: 'value'},
+                success: function(data) {
+                    if (data.response.posts.length < postCount) {
+                        postCount = data.response.posts.length;
+                    }
+                    for ( var i = 0; i < postCount; i++) {
+                        (function(n) {
+                            var full_date = data.response.posts[n].date;
+                            var split_date = full_date.split('-');
+                            var split_day = split_date[2].split(' ');
+                            var year = split_date[0];
+                            var month = split_date[1];
+                            var day = split_day[0];
+                            $('.news .news-feed').append(""+
+                                    "<article>"+
+                                        "<h3 class='post-title'>"+
+                                            data.response.posts[n].title +
+                                        "</h3>" +
 
-/*****
-* Lazy Load
-******/
-$(" img.lazy ").unveil(500);
+                                        "<div class='post-date'>" +
+                                            month + '/' + day + '/' + year +
+                                        "</div>"+
+                                        "<div>" +
+                                            data.response.posts[n].body +
+                                        "</div>" +
+                                        "<div class='post-meta'>tags: "+
+                                            "<span><a target='_blank' href='http://middlewestmgmt.tumblr.com/tagged/" + data.response.posts[n].tags + "''>" + data.response.posts[n].tags + "</span>" +
+                                            "<span><a href=" + data.response.posts[i].post_url + " target='_blank'>permalink</a></span>" +
+                                        "</div>" +
+                                    "</article>"
+                                );
+                            $('.news').fitVids();
+                        })(i);
 
+                    }
 
-
-/*****
-* Mobile Nav
-*****/
-$(" #mobile__menu__button ").click(function() {
-    $("nav ul").toggleClass("mobile__menu--open");
-    return false;
-});
-
-$('.sub-nav ul a').on('click', function() {
-    var  target = $(this).attr('href');
-    $(' nav ul ').removeClass('mobile__menu--open');
-    $('html, body').stop().animate({
-        'scrollTop': $(target).offset().top
-    }, 400, 'swing', function() {
-        window.location.hash = target;
-
-    });
-});
-
-
-
-/*****
-* Mailing List Signup
-******/
-$("#submit").click(function() {
-
-    var email = $("input#email").val();
-    var oid = $("#oid").val();
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-
-    if (email == "") {
-        $('.error--invalid').fadeOut(300);
-        $(".error--empty").fadeIn(300);
-        $("input#email").focus();
-        return false;
-    }
-
-    else if (!emailReg.test(email)) {
-        $(".error--empty").fadeOut(100);
-        $(".error--invalid").fadeIn(300);
-        $("input#email").focus();
-        return false;
-    }
-
-    var dataString = $('.mailing-list form').serialize();
-
-    $.ajax({
-        type: "POST",
-        url: "../../php/mailing-list-submit.php",
-        data: dataString,
-        success: function() {
-            var thanks = "<div class='thanks'>Thanks!</div>";
-            $('.error').hide();
-            $(thanks).hide().appendTo("footer").fadeIn(300).delay(1000).fadeOut(300);
-            $('.mailing-list form').each(function() {
-                this.reset();
+                    $('.news .news-feed').append(readMore);
+                    //$('.news .news-feed').append("<a class='load-more-posts' target='_blank' href='http://middlewestmgmt.tumblr.com/tagged/" + mwmTumblr.tag +"'>Read More Posts</a>");
+                }
             });
         },
-        error: function() {
-            $('footer').append("<div class='submit-error error'>There was an error processing this</div>");
+
+        loadMore: function(tag) {
+            $('.load-more-posts').live('click', function() {
+                var     postCount = $('.news article').length,
+                          offset = postCount,
+                          $this = $(this),
+                          readmore;
+                //$(this).append("<span class='loading'><img src='/img/misc/loading.gif' >");
+                $(this).addClass('loading');
+                $.ajax({
+                    type: 'GET',
+                    dataType: 'jsonp',
+                    url: 'http://api.tumblr.com/v2/blog/middlewestmgmt.tumblr.com/posts?api_key=5cHAhSpCdCiLOdeoWeJAAwCbqFUW4LCvfe9GxwVJXWcgUH4XSl&offset='+ offset + '&tag=' + mwmTumblr.tag,
+                    data: { get_param: 'value'},
+                    success: function(data) {
+                        if ( data.response.blog.posts - offset <= 5 ) {
+                            readmore = "";
+                            console.log("true");
+                        } else {
+                            readmore = "<a class='load-more-posts'>Load More Posts</a>";
+                        }
+                        for (i = 0; i < 5; i++) {
+                            var full_date = data.response.posts[i].date;
+                            var split_date = full_date.split('-');
+                            var split_day = split_date[2].split(' ');
+                            var year = split_date[0];
+                            var month = split_date[1];
+                            var day = split_day[0];
+                            $('.news .scrollable').append(""+
+                                    "<article>"+
+                                    "<h3 class='post-title'>"+
+                                        data.response.posts[i].title +
+                                    "</h3>" +
+
+                                    "<div class='post-date'>" +
+                                        month + '/' + day + '/' + year +
+                                    "</div>"+
+                                    "<div>" +
+                                        data.response.posts[i].body +
+                                    "</div>" +
+                                    "<div class='post-meta'>tags: "+
+                                        "<span><a target='_blank' href='http://tumblr.com/tagged/" + data.response.posts[i].tags + "''>" + data.response.posts[i].tags + "</span>" +
+                                        "<span><a href=" + data.response.posts[i].post_url + " target='_blank'>permalink</a></span>" +
+                                    "</div>" +
+                                "</article>"
+                                );
+                            //$this.fadeOut();
+                            $this.addClass('destroy');
+                        }
+                        $('.news').fitVids();
+                        $('.news .scrollable').append(readmore);
+                    }
+                });
+            });
+
         }
-    });
-    return false;
-});
-
-
-/*****
-* Scrollable Sections
-******/
-// Check inner content height for scrollable sections
-var scrollableContentHeight = $(" .scrollable ").innerHeight();
-
-// If less than 500, collapse and remove overflow prop
-if ( scrollableContentHeight <= 500 ) {
-    $(" .scrollable ").css("overflow-y", "auto");
-} else {
-    $(" .scrollable ").css("overflow-y", "scroll");
-}
-
-// Add faded element when scrolling
-$(" .news .scrollable ").scroll(function() {
-    var elem = $(this);
-    var scroll = $(this).scrollTop();
-
-    //Add bottom border if actively scrolling
-    if (scroll >= 1) {
-        $(this).parent().addClass("bordered").addClass('no-fade');
-    } else {
-        $(this).parent().removeClass("bordered");
     }
 
-});
 
 
-/*****
-* Soundcloud
-******/
-var width = $(window).width();
+    return {
 
-if (width>= 768) {
+        init: function() {
+            console.log(isMobile());
+        },
 
-        $.stratus({
-            links: 'https://soundcloud.com/middlewestmgmt/sets/middlewestmgmt-com'
-        });
+        colorbox: function() {
+            if( isDesk() ) {
+                $(" .colorbox ").colorbox({
+                    rel: "group",
+                    returnFocus: false
+                });
+                $(" .colorbox-extContent ").colorbox({
+                    iframe: true,
+                    width:"80%",
+                    height:"80%",
+                    rel: "group-videos",
+                    transition: "none",
+                    reposition: false
+                });
+            }
+        },
 
-}
+        mobileAlbum: function() {
+            if ( !isDesk() ) {
 
+                $(" .thumb-container ").on('click', function(e) {
+                    var albumInfo = $(this).next();
+                    $(albumInfo).toggleClass("show");
+                    e.preventDefault();
+                });
 
-/*****
-* Tumblr API
-******/
-$.ajax({
-    type: 'GET',
-    dataType: 'jsonp',
-    url: 'http://api.tumblr.com/v2/blog/middlewestmgmt.tumblr.com/posts?api_key=5cHAhSpCdCiLOdeoWeJAAwCbqFUW4LCvfe9GxwVJXWcgUH4XSl&tag=' + mwmTumblr.tag,
-    data: { get_param: 'value'},
-    success: function(data) {
-        if ( data.response.posts.length <= 2) {
-            j = data.response.posts.length;
-        } else {
-            j = 2;
+                $(" .mobile-album-info-close ").on('click', function() {
+                    $(" .mobile-album-info ").removeClass("show");
+                });
+
+            }
+        },
+
+        expander: function() {
+            $(" .expandable ").expander({
+                expandText: "more",
+                userCollapseText: "less",
+                expandSpeed: 400,
+                collapseSpeed: 400,
+                moreClass: "read-more",
+                lessClass: "read-less",
+                slicePoint: 800
+            });
+        },
+
+        flexslider: function() {
+
+            $(" .flexslider ").flexslider({
+                animation: "slide",
+                useCSS: true
+            });
+
+            // Flexsider for album thumbnails when count > 6
+            $(" .flexslider-mini ").flexslider({
+                animation: "slide",
+                slideshow: false,
+                useCSS: true,
+                start: function() {
+                    mwm.colorbox();
+                }
+            });
+
+        },
+
+        lazyload: function() {
+            $(" img.lazy ").unveil(500);
+        },
+
+        mobileNav: function() {
+
+            $mobileNavButton.on("click", function(e) {
+                $navUl.toggleClass('mobile__menu--open');
+                e.preventDefault();
+            });
+
+            $subNavLink.on('click', function() {
+                var  target = $(this).attr('href');
+                $navUl.removeClass('mobile__menu--open');
+                $('html, body').stop().animate({
+                    'scrollTop': $(target).offset().top
+                }, 400, 'swing', function() {
+                    window.location.hash = target;
+                });
+            });
+
+        },
+
+        mailingList: function() {
+            var email = $("input#email").val();
+            var oid = $("#oid").val();
+
+            $(' #submit ').on('click', function(e) {
+                e.preventDefault();
+                var email = $('input#email').val();
+                var oid = $('#oid').val();
+                mailingList(email, oid);
+            });
+        },
+
+        scrollable: function() {
+            $(" .news .scrollable ").scroll(function() {
+                var elem = $(this);
+                var scroll = $(this).scrollTop();
+
+                //Add bottom border if actively scrolling
+                if (scroll >= 1) {
+                    $(this).parent().addClass("bordered").addClass('no-fade');
+                } else {
+                    $(this).parent().removeClass("bordered");
+                }
+            });
+        },
+
+        soundcloud: function() {
+            if ( isDesk() ) {
+                $.stratus({
+                    links: 'https://soundcloud.com/middlewestmgmt/sets/middlewestmgmt-com'
+                });
+            }
+        },
+
+        tumblr: function() {
+            var tag = mwmTumblr.tag;
+            var page = $(' body ').attr('class');
+            tumblr.init(tag, page);
+            tumblr.loadMore();
         }
-        for (i = 0; i < j; i++) {
-            var full_date = data.response.posts[i].date;
-            var split_date = full_date.split('-');
-            var split_day = split_date[2].split(' ');
-            var year = split_date[0];
-            var month = split_date[1];
-            var day = split_day[0];
-            $('.news .news-feed').append(""+
-                    "<article>"+
-                        "<h3 class='post-title'>"+
-                            data.response.posts[i].title +
-                        "</h3>" +
+    };
 
-                        "<div class='post-date'>" +
-                            month + '/' + day + '/' + year +
-                        "</div>"+
-                        "<div>" +
-                            data.response.posts[i].body +
-                        "</div>" +
-                        "<div class='post-meta'>tags: "+
-                            "<span><a target='_blank' href='http://tumblr.com/tagged/" + data.response.posts[i].tags + "''>" + data.response.posts[i].tags + "</span>" +
-                            "<span><a href=" + data.response.posts[i].post_url + " target='_blank'>permalink</a></span>" +
-                        "</div>" +
-                    "</article>"
-                );
-            $('.news').fitVids();
-        }
 
-        $('.news .news-feed').append("<a class='load-more-posts' target='_blank' href='http://middlewestmgmt.tumblr.com/tagged/" + mwmTumblr.tag +"'>Read More Posts</a>");
-    }
+}(window.jQuery, window, document));
+
+$(function() {
+    mwm.colorbox();
+    mwm.mobileAlbum();
+    mwm.expander();
+    mwm.flexslider();
+    mwm.lazyload();
+    mwm.mobileNav();
+    mwm.mailingList();
+    mwm.scrollable();
+    mwm.soundcloud();
+    mwm.tumblr();
 });
 
-});
